ef6e9d1e5b7017773fc9306e1ca64300b495ee14
Maikon-Santos <maikoon-santoz@gmail.com>
2022-09-28 17:05:03 -0300

    introdução ao react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://i.pinimg.com/originals/39/12/ec/3912ec9ec5b12ccbc6b99f92fdd81b33.png"
+             alt="tela inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
